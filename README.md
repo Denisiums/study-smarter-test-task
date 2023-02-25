0b347c80a0a35fe324e6cc0d3a99e27457df13be
Denis Iliukhin <denisiums@gmail.com>
2023-02-25 15:44:53 +0100

    dashed border. Readme updated

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # StudySmarterTestTask
 
-## Development server
+The task:
+```
+As a preparation, we would ask you to implement the attached Mockup as an Angular component. The component should display the weekdays and highlight the current day.
+```
+The mockup is located in `src/app/assets`
+
+The solution does not support IE11.
+
+## How to run
 
 Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
--- src/app/weekdays/weekdays.component.ts
+++ src/app/weekdays/weekdays.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-weekdays',
   templateUrl: './weekdays.component.html',
   styleUrls: ['./weekdays.component.scss']
 })
-export class WeekdaysComponent {
+export class WeekdaysComponent implements OnInit {
+  // in bigger project we would have moment.js or date-fns to operate and format dates in a more convenient way,
+  // but it doesn't worth having another dependency here,
+  // also we don't track user locale (because 1st day of the week can differ)
+  currentDay = 0;
+  daysText: {[key: string]: string} = {
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
+  };
+  weekDays = [1, 2, 3, 4, 5, 6, 0];
 
+
+  ngOnInit() {
+    // we consider there is no requirement to keep the weekday fresh after page load
+    this.setCurrentDay();
+  }
+
+  private setCurrentDay(): void {
+    const now = new Date();
+    this.currentDay = now.getUTCDay();
+  }
 }
