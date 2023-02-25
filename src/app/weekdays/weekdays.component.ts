7a3179fd3c0bbb028450a7d9e7f2f6fbad601e8b
Denis Iliukhin <denisiums@gmail.com>
2023-02-25 11:06:24 +0100

    basic project structure, css reset

---
+++ src/app/weekdays/weekdays.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekdays',
+  templateUrl: './weekdays.component.html',
+  styleUrls: ['./weekdays.component.scss']
+})
+export class WeekdaysComponent {
+
+}
